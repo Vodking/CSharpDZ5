d6e88a84b7b3dfd2b7f5c3adbf7fcddeed36e080
Top <Top@WIN-PCSPK6SN8F0>
2026-04-23 11:21:52 +0500

    s

---
+++ CSharpDZ5/Matrix.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpDZ5
+{
+    public class Matrix
+    {
+        private double[,] _values;
+
+        private int _grade;
+
+        public int Grade
+        {
+            get {  return _grade; }
+        }
+        public int Rows { get; }
+        public int Cols { get; }
+
+        public Matrix(double[,] values, int rows, int cols)
+        {
+            values = new double[rows, cols];
+
+            Rows = rows; 
+            Cols = cols;
+
+            if(rows == cols)
+            {
+                _grade = cols;
+            }
+            else if(rows > cols)
+            {
+                _grade = cols;
+            }
+            else
+            {
+                _grade = rows;
+            }
+        }
+
+        public double this[int r, int c]
+        {
+            get
+            {
+                if (r >= 0 && r < Rows && c >= 0 && c < Cols)
+                    return _values[r, c];
+                else
+                    Console.WriteLine("Проблемы с диапазоном! -1");
+                return -1;
+            }
+            set
+            {
+                if (r >= 0 && r < Rows && c >= 0 && c < Cols)
+                    _values[r, c] = value;
+                else
+                    Console.WriteLine("Проблемы с диапазоном!");
+            }
+        }
+
+        public static Matrix operator *(Matrix left, Matrix right)
+        {
+            int leastRows, leastCols;
+            double[,] temp;
+            if(left.Cols != right.Rows)
+            {
+                temp = new double[1, 1] { { -1 } };
+                return new Matrix(temp, 1, 1);
+            }
+            if(left.Rows > right.Rows)
+            {
+                leastRows = right.Rows;
+            }
+            else
+            {
+                leastRows= left.Rows;
+            }
+
+            if(right.Cols > left.Cols)
+            {
+                leastCols = left.Cols;
+            }
+            else
+            {
+                leastCols = right.Cols;
+            }
+
+            temp = new double[leastRows, leastCols];
+
+            double num;
+
+            for(int i = 0; i < leastRows; i++)
+            {
+                for(int j = 0; j < leastCols; j++)
+                {
+                    for (int k = 0; k < leastRows; k++)
+                    {
+                        for(int l = 0; l < leastCols; l++)
+                        {
+                            num += left[k, l] * right[k, l];
+                        }
+                    }
+                }
+            }
+
+        }
+    }
+}
--- CSharpDZ5/Program.cs
+++ CSharpDZ5/Program.cs
@@ -1,19 +1,16 @@
-﻿namespace CSharpDZ5
+﻿using System.Data;
+
+namespace CSharpDZ5
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int userInput;
-            
-            while(true)
-            {
-                Console.WriteLine("Урок 5\n");
-                do
-                {
-                    Console.WriteLine()
-                }while(true)
-            }
+            int grade;
+
+            Console.Write("Введите разряд матрицы");
+            grade = Convert.ToInt32(Console.ReadLine());
+
         }
     }
 }
